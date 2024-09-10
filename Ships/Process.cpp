fa70262988c85c4bff774c167afa572e109ef972
Maciej Cieslik <maciek.cieslik123@gmail.com>
2024-09-10 22:31:28 +0200

    added islands vector

--- Ships/Process.cpp
+++ Ships/Process.cpp
@@ -35,20 +35,23 @@ void Process::initializeNewGame(std::vector<std::string>& playerNames, const int
 	if (playerNames.size() < 2) throw std::invalid_argument(langOptions->getCommunicate("process_invalid_player_number"));
 
     std::vector<std::shared_ptr<Ship>> shipsPlayer1;
-    shipsPlayer1.push_back(std::make_shared<Destroyer>('A', std::vector<Coords>({Coords(6, 6), Coords(7, 6)}), maxBoardSize));
-    shipsPlayer1.push_back(std::make_shared<Cruiser>('B', std::vector<Coords>({Coords(5, 3), Coords(6, 3), Coords(7, 3)}), maxBoardSize));
+    shipsPlayer1.push_back(std::make_shared<Destroyer>('A', std::vector<Coords>({Coords(9, 11), Coords(10, 11)}), maxBoardSize));
+    shipsPlayer1.push_back(std::make_shared<Cruiser>('B', std::vector<Coords>({Coords(4, 4), Coords(5, 4), Coords(6, 4)}), maxBoardSize));
     std::shared_ptr<Player> player_1 = std::make_shared<Player>(playerNames[0], shipsPlayer1, langOptions);
 
     std::vector<std::shared_ptr<Ship>> shipsPlayer2;
-    shipsPlayer2.push_back(std::make_shared<Destroyer>('a', std::vector<Coords>({Coords(3, 6), Coords(4, 6)}), maxBoardSize));
-    shipsPlayer2.push_back(std::make_shared<Cruiser>('b', std::vector<Coords>({Coords(5, 8), Coords(6, 8), Coords(7, 8)}), maxBoardSize));
+    shipsPlayer2.push_back(std::make_shared<Destroyer>('a', std::vector<Coords>({Coords(2, 6), Coords(3, 6)}), maxBoardSize));
+    shipsPlayer2.push_back(std::make_shared<Cruiser>('b', std::vector<Coords>({Coords(5, 6), Coords(6, 6), Coords(7, 6)}), maxBoardSize));
     std::shared_ptr<Player> player_2 = std::make_shared<Player>(playerNames[1], shipsPlayer2, langOptions);
 
     std::vector<std::shared_ptr<Player>> players = {player_1, player_2};
 
-    std::shared_ptr<Island> island = std::make_shared<Island>(std::vector<Coords>({Coords(5, 5), Coords(6, 5), Coords(10, 5)}), 2, maxBoardSize); 
+    std::shared_ptr<Island> island2 = std::make_shared<Island>(std::vector<Coords>({Coords(5, 5), Coords(6, 5), Coords(10, 5)}), 3, maxBoardSize);
+    std::shared_ptr<Island> island1 = std::make_shared<Island>(std::vector<Coords>({Coords(3, 7), Coords(3, 8), Coords(3, 9)}), 2, maxBoardSize); 
+    std::shared_ptr<Island> island0 = std::make_shared<Island>(std::vector<Coords>({Coords(2, 7), Coords(2, 8), Coords(4, 11), Coords(5, 11)}), 0, maxBoardSize);  
+    std::vector<std::shared_ptr<Island>> islands = {island0, island1, island2};
 
-    game = std::make_unique<Game>(players, island, maxBoardSize, langOptions);
+    game = std::make_unique<Game>(players, islands, maxBoardSize, langOptions);
 
     game->turnGameOn();
 
