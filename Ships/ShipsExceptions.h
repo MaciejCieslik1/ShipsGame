11e89054e37d57bd3fdb8b74941d22ba53eb5a64
Maciej Cieslik <maciek.cieslik123@gmail.com>
2024-09-17 22:07:13 +0200

    change the view of code

--- Ships/ShipsExceptions.h
+++ Ships/ShipsExceptions.h
@@ -10,110 +10,137 @@ public:
 	const char* what() const noexcept override;
 };
 
-class object_already_contained : public ShipsException {
+class object_already_contained : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class index_out_of_range : public ShipsException {
+class index_out_of_range : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class no_object_found : public ShipsException {
+class no_object_found : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class font_load_error : public ShipsException {
+class font_load_error : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_board_size : public ShipsException {
+class invalid_board_size : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_field_size : public ShipsException {
+class invalid_field_size : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class field_out_of_range : public ShipsException {
+class field_out_of_range : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class field_not_found : public ShipsException {
+class field_not_found : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class field_not_empty : public ShipsException {
+class field_not_empty : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_coordinate : public ShipsException {
+class invalid_coordinate : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_range_value : public ShipsException {
+class invalid_range_value : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_damage_value : public ShipsException {
+class invalid_damage_value : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_missle_id : public ShipsException {
+class invalid_missle_id : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_player_index : public ShipsException {
+class invalid_player_index : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_winner_player : public ShipsException {
+class invalid_winner_player : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_line_in_file : public ShipsException {
+class invalid_line_in_file : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class key_not_found : public ShipsException {
+class key_not_found : public ShipsException
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_max_moves_value : public ShipsException {
+class invalid_max_moves_value : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_movement_value : public ShipsException {
+class invalid_movement_value : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_hit_point_value : public ShipsException {
+class invalid_hit_point_value : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_name : public ShipsException {
+class invalid_name : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class improper_vector_size : public ShipsException {
+class improper_vector_size : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class space_in_name_error : public ShipsException {
+class space_in_name_error : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_height : public ShipsException {
+class invalid_height : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class field_occupied : public ShipsException {
+class field_occupied : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class file_failed_string_to_int_convertion : public ShipsException {
+class file_failed_string_to_int_convertion : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
 
-class invalid_file_format : public ShipsException {
+class invalid_file_format : public ShipsException 
+{
 	using ShipsException::ShipsException;
 };
